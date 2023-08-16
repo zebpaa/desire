bd9517bd720d31fe4847e9dcb5f902243d77ab67
zebpaa <skvorcov_90@mail.ru>
2023-08-16 11:41:15 +0400

    fully adapted index.html

--- app/js/main.js
+++ app/js/main.js
@@ -10,6 +10,10 @@ $(function () {
     $(".menu").toggleClass("menu--open");
   });
 
+  if($(window).width() < 651){
+    $('.works-path__item--measuring').appendTo($('.works-path__items-box'));
+  }
+
   $(".top__slider").slick({
     dots: true,
     arrows: false,
